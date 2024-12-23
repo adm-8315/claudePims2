ca852ddfa1c4859d3ba08c4507d4f3d841b1ad22
adm-8315 <rlram78812@gmail.com>
2024-12-22 21:16:53 -0500

    Update main URLs configuration

--- config/urls.py
+++ config/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('apps.users.urls')),
+    path('core/', include('apps.core.urls')),
+    path('inventory/', include('apps.inventory.urls')),
+    path('production/', include('apps.production.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += [
-        path('__debug__/', include('debug_toolbar.urls')),
-    ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
