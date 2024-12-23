e4df2406e05c05c7b3849cd3c506accc489d540e
adm-8315 <rlram78812@gmail.com>
2024-12-22 20:20:45 -0500

    Add Django project core structure

--- manage.py
+++ manage.py
@@ -3,8 +3,10 @@
 import os
 import sys
 
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pims2.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -12,6 +14,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
