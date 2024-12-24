7ef2f5fa162de0e1967b4f5615abf940e8cf7376
adm-8315 <rlram78812@gmail.com>
2024-12-24 08:13:26 -0500

    Add core models

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Core models will be added here
+class Address(models.Model):
+    addressid = models.AutoField(db_column='addressID', primary_key=True)
+    lineone = models.CharField(db_column='lineOne', max_length=255)
+    linetwo = models.CharField(db_column='lineTwo', max_length=255, blank=True, null=True)
+    linethree = models.CharField(db_column='lineThree', max_length=255, blank=True, null=True)
+    city = models.ForeignKey('City', models.DO_NOTHING, db_column='city')
+    zip = models.CharField(max_length=6)
+    state = models.ForeignKey('State', models.DO_NOTHING, db_column='state')
+
+    class Meta:
+        db_table = 'address'
+
+class City(models.Model):
+    cityid = models.AutoField(db_column='cityID', primary_key=True)
+    city = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'city'
+
+class State(models.Model):
+    stateid = models.AutoField(db_column='stateID', primary_key=True)
+    state = models.CharField(max_length=255)
+    short = models.CharField(max_length=2)
+
+    class Meta:
+        db_table = 'state'
+
+# Add remaining core models...
